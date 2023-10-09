519e82ade58d0cad4e651321680c4ac10f540867
Kael Adair <kja75810@gmail.com>
2023-10-09 12:27:47 -0700

    Firebase config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import FirebaseInit from './components/FirebaseInit.js';
 
 function App() {
   return (
     <div className="App">
+      <FirebaseInit />
       <h1>React App</h1>
     </div>
   );
---
+++ src/components/FirebaseInit.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRUW7WNENXmxGdhEofeGEx9AORUIAzDCA",
+  authDomain: "test-project-4b094.firebaseapp.com",
+  projectId: "test-project-4b094",
+  storageBucket: "test-project-4b094.appspot.com",
+  messagingSenderId: "827479118190",
+  appId: "1:827479118190:web:2a16a8737aa60ce8f5b17f",
+  measurementId: "G-MECPTP4N93"
+};
+
+const FirebaseInit = () => {
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }, []);
+
+  return null;
+};
+
+export default FirebaseInit;
